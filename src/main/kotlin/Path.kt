383c56f8aafdbf06fe3e0d245a0feec9dbc099bd
OShane-McKenzie <oshanemckenzie5@gmail.com>
2023-12-11 20:21:07 -0500

    First commit

--- src/main/kotlin/Path.kt
+++ src/main/kotlin/Path.kt
@@ -3,4 +3,5 @@ object Path {
     val home: String = System.getProperty("user.home")
     val config:String = "$home/.config/ArchWeaverCompose"
     val logs:String = "$home/ArchWeaverCompose/logs"
+
 }
